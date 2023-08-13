b7a384367e747b86a559eab060e7d5e8a9a513d0
QuiteriaAlves <qteixeira@yahoo.com.br>
2023-08-13 18:16:10 -0300

    Configurações

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
   fixturesFolder: false,
   video: false,
   screenshotOnRunFailure: false
